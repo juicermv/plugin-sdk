3aee1bfbfac78252cd4e64da59a6a811a2b9ada1
juicermv <yonimeller@protonmail.com>
2025-07-26 23:10:40 +0300

    Update project.lua

--- project.lua
+++ project.lua
@@ -62,143 +62,142 @@ local mingw_flags_linker = {
     "--gc-sections"
 }
 
-local plugin_sa = Project(
-    "plugin_sa",
-    "c++",
-    "plugin.lib",
+-- GTA SA
+local plugin_sa = new_project("plugin_sa", "CPP")
+plugin_sa:file(AddTables(
+    filesystem:walk("hooking", false, filter),
     AddTables(
-        filesystem:walk("hooking", false, filter),
-        AddTables(
-            filesystem:walk("shared", true, filter),
-            filesystem:walk("plugin_sa", true, filter)
-        )
-    ),
-    nil,
-    AddTables(Get3DGameDirs("sa"), shared_paths),
-    shared_paths,
-    nil,
-    {
-        "_HAS_CXX17",
-        "_CRT_SECURE_NO_WARNINGS",
-        "_CRT_NON_CONFORMING_SWPRINTFS",
-        "_USE_MATH_DEFINES",
-        "_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING",
-        "_DX9_SDK_INSTALLED",
-        "GTASA",
-        "PLUGIN_SGV_10US",
-        "RW"
-    },
-    msvc_flags_compiler,
-    msvc_flags_linker,
-    nil,
-    nil,
-    "x86",
-    "staticlibrary"
-)
+        filesystem:walk("shared", true, filter),
+        filesystem:walk("plugin_sa", true, filter)
+    )
+))
+    
+plugin_sa:include(AddTables(Get3DGameDirs("sa"), shared_paths))
+plugin_sa:lib_path(shared_paths)
+plugin_sa:define({
+    "_HAS_CXX17",
+    "_CRT_SECURE_NO_WARNINGS",
+    "_CRT_NON_CONFORMING_SWPRINTFS",
+    "_USE_MATH_DEFINES",
+    "_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING",
+    "_DX9_SDK_INSTALLED",
+    "GTASA",
+    "PLUGIN_SGV_10US",
+    "RW"
+})
 
-local plugin_3 = Project(
-    "plugin_3",
-    "c++",
-    "plugin_iii.lib",
+plugin_sa:type("StaticLibrary")
+
+
+-- GTA 3
+local plugin_3 = new_project("plugin_3", "CPP")
+
+plugin_3:file(AddTables(
+    filesystem:walk("hooking", false, filter),
     AddTables(
-        filesystem:walk("hooking", false, filter),
-        AddTables(
-            filesystem:walk("shared", true, filter),
-            filesystem:walk("plugin_III", true, filter)
-        )
-    ),
-    nil,
-    AddTables(Get3DGameDirs("III"), shared_paths),
-    shared_paths,
-    nil,
-    {
-        "_HAS_CXX17",
-        "_CRT_SECURE_NO_WARNINGS",
-        "_CRT_NON_CONFORMING_SWPRINTFS",
-        "_USE_MATH_DEFINES",
-        "_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING",
-        "_DX9_SDK_INSTALLED",
-        "GTA3",
-        "PLUGIN_SGV_10EN",
-        "RW"
-    },
-    msvc_flags_compiler,
-    msvc_flags_linker,
-    nil,
-    nil,
-    "x86",
-    "staticlibrary"
-)
+        filesystem:walk("shared", true, filter),
+        filesystem:walk("plugin_III", true, filter)
+    )
+))
 
-local plugin_vc = Project(
-    "plugin_vc",
-    "c++",
-    "plugin_vc.lib",
+plugin_3:include(AddTables(Get3DGameDirs("III"), shared_paths))
+plugin_3:lib_path(shared_paths)
+
+plugin_3:define({
+    "_HAS_CXX17",
+    "_CRT_SECURE_NO_WARNINGS",
+    "_CRT_NON_CONFORMING_SWPRINTFS",
+    "_USE_MATH_DEFINES",
+    "_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING",
+    "_DX9_SDK_INSTALLED",
+    "GTA3",
+    "PLUGIN_SGV_10EN",
+    "RW"
+})
+
+plugin_3:type("StaticLibrary")
+
+-- GTA VC
+local plugin_vc = new_project("plugin_vc", "CPP")
+plugin_vc:file(
     AddTables(
         filesystem:walk("hooking", false, filter),
         AddTables(
             filesystem:walk("shared", true, filter),
             filesystem:walk("plugin_vc", true, filter)
         )
-    ),
-    nil,
-    AddTables(Get3DGameDirs("vc"), shared_paths),
-    shared_paths,
-    nil,
-    {
-        "_HAS_CXX17",
-        "_CRT_SECURE_NO_WARNINGS",
-        "_CRT_NON_CONFORMING_SWPRINTFS",
-        "_USE_MATH_DEFINES",
-        "_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING",
-        "_DX9_SDK_INSTALLED",
-        "GTAVC",
-        "PLUGIN_SGV_10EN",
-        "RW"
-    },
-    msvc_flags_compiler,
-    msvc_flags_linker,
-    nil,
-    nil,
-    "x86",
-    "staticlibrary"
+    )
 )
+plugin_vc:include(AddTables(Get3DGameDirs("vc"), shared_paths))
+plugin_vc:lib_path(shared_paths)
 
-function MSVC_SA() 
+plugin_vc:define({
+    "_HAS_CXX17",
+    "_CRT_SECURE_NO_WARNINGS",
+    "_CRT_NON_CONFORMING_SWPRINTFS",
+    "_USE_MATH_DEFINES",
+    "_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING",
+    "_DX9_SDK_INSTALLED",
+    "GTAVC",
+    "PLUGIN_SGV_10EN",
+    "RW"
+})
+
+
+plugin_vc:type("StaticLibrary")
+
+
+function MSVC_SA()
+    plugin_sa:output("plugin.lib")
+    plugin_sa:flag("Compiler", msvc_flags_compiler)
+    plugin_sa:flag("Linker", msvc_flags_linker)
+    plugin_sa:arch("x86")
+
     msvc:build(plugin_sa)
 end
 
 function MSVC_III() 
+    plugin_3:output("plugin_iii.lib")
+    plugin_3:flag("Compiler", msvc_flags_compiler)
+    plugin_3:flag("Linker", msvc_flags_linker)
+    plugin_3:arch("x86")
+
     msvc:build(plugin_3)
 end
 
-function MSVC_VC() 
+function MSVC_VC()
+    plugin_vc:output("plugin_vc.lib")
+    plugin_vc:flag("Compiler", msvc_flags_compiler)
+    plugin_vc:flag("Linker", msvc_flags_linker)
+    plugin_vc:arch("x86")
+
     msvc:build(plugin_vc)
 end
 
 function MINGW_SA()
-    plugin_sa.arch = "i686"
-    plugin_sa.compiler_flags = mingw_flags_compiler
-    plugin_sa.linker_flags = mingw_flags_linker
-    plugin_sa.output = "libplugin.a"
+    plugin_sa:arch("i686")
+    plugin_sa:flag("Compiler", mingw_flags_compiler)
+    plugin_sa:flag("Linker", mingw_flags_linker)
+    plugin_sa:output("libplugin.a")
 
     mingw:build(plugin_sa)
 end
 
 function MINGW_III()
-    plugin_3.arch = "i686"
-    plugin_3.compiler_flags = mingw_flags_compiler
-    plugin_3.linker_flags = mingw_flags_linker
-    plugin_3.output = "libplugin_iii.a"
+    plugin_3:arch("i686")
+    plugin_3:flag("Compiler", mingw_flags_compiler)
+    plugin_3:flag("Linker", mingw_flags_linker)
+    plugin_3:output("libplugin_iii.a")
 
     mingw:build(plugin_3)
 end
 
 function MINGW_VC()
-    plugin_vc.arch = "i686"
-    plugin_vc.compiler_flags = mingw_flags_compiler
-    plugin_vc.linker_flags = mingw_flags_linker
-    plugin_vc.output = "libplugin_vc.a"
+    plugin_vc:arch("i686")
+    plugin_vc:flag("Compiler", mingw_flags_compiler)
+    plugin_vc:flag("Linker", mingw_flags_linker)
+    plugin_vc:output("libplugin_vc.a")
+
     mingw:build(plugin_vc)
 end
 
